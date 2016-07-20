291a958558ef1f2b475370ad4cd0ca9b7426b5b2
VitTokarev <vtokarev@bk.ru>
2016-07-20 14:59:25 +0300

    убрал код для поиска ошибок

--- controller/controller_realty.php
+++ controller/controller_realty.php
@@ -15,25 +15,9 @@ class ControllerRealty
 	{
 		$realty = Realty::all_lines(); //function_realty.php
 		
-		//echo '<br><br>';// ERR
-		//echo 'controller_realty.php: function all_lines_controller() Realty::all_lines() =';//
-		//print_r($realty);//
-		//echo '<br><br>';
-		
 		$types = RealtyType::all_lines();
 		
-		//echo '<br><br>';// OK
-		//echo 'controller_realty.php: function all_lines_controller() RealtyType::all_lines() =';//
-		//var_dump($types);//
-		//echo '<br><br>';
-		
-		$types = RealtyType::type_id_array($types);
-		
-		echo '<br><br>';// ERR
-		echo 'controller_realty.php: function all_lines_controller() RealtyType::type_id_array($types) =';//
-		var_dump($types);//
-		echo '<br><br>';
-		
+		$types = RealtyType::type_id_array($types);		
 		
 		return render("index_content",['realty' => $realty,
 									   'realty_type' => $types
