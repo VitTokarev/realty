5e80c1aba58b5ccc3d225c104e27d40aa5a36dee
VitTokarev <vtokarev@bk.ru>
2016-07-24 23:06:21 +0300

    обновленная ДБ

--- db_realty.sql
+++ db_realty.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 04 2016 г., 16:55
+-- Время создания: Июл 24 2016 г., 23:04
 -- Версия сервера: 5.5.45
 -- Версия PHP: 5.4.44
 
@@ -35,18 +35,21 @@ CREATE TABLE IF NOT EXISTS `realty` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `type_id` (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=61 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=68 ;
 
 --
 -- Дамп данных таблицы `realty`
 --
 
 INSERT INTO `realty` (`id`, `type_id`, `title`, `address`, `price`) VALUES
-(33, 3, 'Комната в коммунальной квартире, туалет совмещенный, одни соседи алканавты, другие многодетная семья. И далее может быть длинное описание. Так как описание может быть очччень длинным, его нет смысла полностью выводить на главной странице. Полностью выводи', 'ул. Строителей 70', 2000000),
-(34, 1, '3-комнатная квартира', 'Средний пр. 45', 7500000),
+(34, 3, '3-комнаты', 'Средний пр. 45', 7500000),
 (50, 2, '2-этажный дом', 'Село Крайнее Новая ул 20', 3000000),
 (51, 3, 'Комната в коммунальной квартире', 'Средняя 60', 700000),
-(58, 1, '3-комнатная квартира', 'Столичная ул 9', 7000000);
+(58, 1, '3-комнатная квартира', 'Столичная ул 9', 7000000),
+(62, 2, 'с домом впридачу', 'Село Васюки 4', 50),
+(63, 6, 'Суперсарай', 'Супердеревня', 300000),
+(64, 3, 'комната', 'старая улица', 1000000),
+(66, 3, 'в коммунальной квартире', 'старая ул 5', 3000000);
 
 -- --------------------------------------------------------
 
@@ -55,22 +58,24 @@ INSERT INTO `realty` (`id`, `type_id`, `title`, `address`, `price`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `realty_type` (
-  `type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`type_id`),
-  UNIQUE KEY `type_id_3` (`type_id`),
-  KEY `type_id` (`type_id`),
-  KEY `type_id_2` (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `type_id_3` (`id`),
+  KEY `type_id` (`id`),
+  KEY `type_id_2` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=28 ;
 
 --
 -- Дамп данных таблицы `realty_type`
 --
 
-INSERT INTO `realty_type` (`type_id`, `type`) VALUES
+INSERT INTO `realty_type` (`id`, `title`) VALUES
 (1, 'Квартира'),
 (2, 'Загородный дом'),
-(3, 'Комната');
+(3, 'Комната'),
+(6, 'Суперсарай'),
+(27, 'к');
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -80,7 +85,7 @@ INSERT INTO `realty_type` (`type_id`, `type`) VALUES
 -- Ограничения внешнего ключа таблицы `realty`
 --
 ALTER TABLE `realty`
-  ADD CONSTRAINT `realty_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `realty_type` (`type_id`);
+  ADD CONSTRAINT `realty_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `realty_type` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
