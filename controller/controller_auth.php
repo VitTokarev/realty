53785fa4fba8ca70d3fc72ccd06bd0c752bbf593
VitTokarev <vtokarev@bk.ru>
2016-07-29 12:03:13 +0300

    контроллер для авторизации

---
+++ controller/controller_auth.php
@@ -0,0 +1,41 @@
+<?php
+
+class ControllerAuth
+{
+    function __call($name, $params)
+    {
+        e404();
+    }
+
+    function login_controller()
+    {
+
+        if (count($_POST)) {
+            if ($_POST['action'] === 'login') {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $user = new User();
+
+                if ($user->auth($username,$password)) {
+                    header("Location: /less-authorization/realty.git/index.php");
+                    die();
+
+                }
+                else
+                {
+                    header("Location: index.php?cat=auth&view=login");
+                    die();
+                }
+
+            }
+        }
+
+        return render('auth/login', [
+
+        ]);
+
+    }
+
+
+}
