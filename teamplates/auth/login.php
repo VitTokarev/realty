09e8ef47ad74a77d68943062f6ae34a772ebf7a1
VitTokarev <vtokarev@bk.ru>
2016-07-29 12:05:09 +0300

    вьюшка для авторизации

---
+++ teamplates/auth/login.php
@@ -0,0 +1,32 @@
+
+
+<!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+               <div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Авторизация</h1>
+        <form action="" method="post">
+            <input type="hidden" name="action" value="login"/>
+            <div class="form-group input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" placeholder="Имя пользователя" class="form-control" name="username">
+            </div>
+            <div class="form-group input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="password" placeholder="Пароль" class="form-control" name="password">
+            </div>
+
+
+            <button class="btn btn-success" type="submit">Войти</button>
+        </form>
+
+    </div>
+    <!-- /.col-lg-12 -->
+
+</div>
+<!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
