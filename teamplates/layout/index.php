0f8ba21bb04d14a4d274fe47f39501959f43f0f5
VitTokarev <vtokarev@bk.ru>
2016-07-29 11:59:54 +0300

    в меню добавил пункт пользователи

--- teamplates/layout/index.php
+++ teamplates/layout/index.php
@@ -66,9 +66,11 @@
                             <a href="index.php?redirect=add_line"> Добавление объекта недвижимости</a>
                         </li>
                         <li>
-                            <a href="index.php?redirect=all_types&controller=controller_realty_type"> Редактирование типов объектов недвижимости</a>
+                            <a href="index.php?redirect=all_types&controller=controller_realty_type">Редактирование типов объектов недвижимости</a>
                         </li>
                         <li>
+							<a href="index.php?redirect=users_list&controller=controller_users">Пользователи</a>
+						</li>
 
                     </ul>
                 </div>
