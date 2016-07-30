fa1250bdac1061451b639336585e13606ec74f80
VitTokarev <vtokarev@bk.ru>
2016-07-30 03:09:14 +0300

    Пункты навигации доступные только администратору

---
+++ teamplates/layout/navbar-admin.php
@@ -0,0 +1,8 @@
+
+                        <li>
+                            <a href="index.php?redirect=all_types&controller=controller_realty_type">Редактирование типов объектов недвижимости</a>
+                        </li>
+                        <li>
+							<a href="index.php?redirect=users_list&controller=controller_users">Пользователи</a>
+						</li>';
+						
